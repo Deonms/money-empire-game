21eb81dfbbce04c183a8a171135da44704406adb
DeonsGaming <Deon.mob@gmail.com>
2026-05-13 09:53:51 +0200

    added some code

--- Animals.cs
+++ Animals.cs
@@ -8,8 +8,15 @@ namespace Money_Empire_Game
 {
     internal class Animals
     {
-        private string _eyecolor;
-        public string Eyecolor;
-        protected string Eyecolor2;
+        private string _eyeColor;
+        public string EyeColor;
+        protected string EyeColor2
+        {
+            get => _eyeColor;
+            private set
+            {
+                _eyeColor = value;
+            }
+        }
     }
 }
