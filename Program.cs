ab0519866864938cc4243b6ff8e02a6143838a25
DeonsGaming <Deon.mob@gmail.com>
2026-05-13 09:48:08 +0200

    added something

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Empire_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //s
+        }
+    }
+}
---
+++ Work.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Empire_Game
+{
+    internal class Work
+    {
+
+    }
+}
