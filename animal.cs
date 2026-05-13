67b80e8a6dea2ad0243da13788ab82d11a18e013
DeonsGaming <Deon.mob@gmail.com>
2026-05-13 09:48:37 +0200

    added somethings else

---
+++ animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Empire_Game
+{
+    internal class animal
+    {
+    }
+}
