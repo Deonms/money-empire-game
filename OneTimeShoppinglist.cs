688d4aad86ebe22f79b8b0c5c54aa1b9f60ed2cd
DeonsGaming <Deon.mob@gmail.com>
2026-06-01 11:01:29 +0200

    changed somethings

---
+++ OneTimeShoppinglist.cs
@@ -0,0 +1,9 @@
+﻿namespace Money_Empire_Game
+{
+    internal class OneTimeShoppinglist : OvereverenShop
+    {
+        public OneTimeShoppinglist(string nameOfItem, float costOfItem, string infoOfItem, float multiplieradd, float autoincome) : base(nameOfItem, costOfItem, infoOfItem, multiplieradd, autoincome)
+        {
+        }
+    }
+}
